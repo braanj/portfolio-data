f15b47fce7f39512951223f8e70868f6f15a57cb
Anjjar Brahim <anjar.bra@gmail.com>
2023-03-01 22:55:09 +0100

    Initialize project

---
+++ src/api/expercience/controllers/expercience.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * expercience controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::expercience.expercience');
---
+++ src/api/expercience/services/expercience.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * expercience service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::expercience.expercience');
